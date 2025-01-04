03c16cbbbb735a26cf67873515f1bed56becd1be
Matthew McNee <matt@acyleris.com>
2025-01-04 15:35:51 +0000

    Style homepage with dataset displays

--- components/ui/ShapeIconClickable.tsx
+++ components/ui/ShapeIconClickable.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import circleIcon from "@/public/circle.svg";
-import squareIcon from "@/public/square.svg";
-import triangleIcon from "@/public/triangle.svg";
-import diamondIcon from "@/public/diamond.svg";
-import pentagonIcon from "@/public/pentagon.svg";
-import hexagonIcon from "@/public/hexagon.svg";
+import circleIcon from "@/public/icons/shapes/circle.svg";
+import squareIcon from "@/public/icons/shapes/square.svg";
+import triangleIcon from "@/public/icons/shapes/triangle.svg";
+import diamondIcon from "@/public/icons/shapes/diamond.svg";
+import pentagonIcon from "@/public/icons/shapes/pentagon.svg";
+import hexagonIcon from "@/public/icons/shapes/hexagon.svg";
 import Image from "next/image";
 
 import styles from "./ShapeIcon.module.scss";
