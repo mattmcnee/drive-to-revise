74bf8170131d17dc03d17a235dc66d91e551f65b
Matthew McNee <matt@acyleris.com>
2025-01-06 21:22:31 +0000

    Add user profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Navbar from "@/components/ui/Navbar";
+import { useAuth } from "@/firebase/useAuth";
+import { getPaginatedDatasets, DatasetDocument, getUsername } from "@/firebase/firestoreInterface";
+import DatasetCard from "@/components/ui/DatasetCard";
+import styles from "./page.module.scss";
+
+const ProfilePage = () => {
+  const { user, loading } = useAuth();
+  const [datasets, setDatasets] = useState<DatasetDocument[]>([]);
+  const [username, setUsername] = useState<string | null>(null);
+
+  if (!loading && !user) {
+    window.location.href = "/login";
+  }
+
+  useEffect(() => {
+    if (user) {
+      getPaginatedDatasets(9, "", user.uid).then((newDatasets) => {
+        setDatasets(newDatasets.datasets);
+      });
+
+      getUsername(user.uid).then((newUsername) => {
+        setUsername(newUsername);
+      });
+    }
+  }, [user, loading]);
+
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.profilePage}>
+        <h2 className={styles.pageTitle}>Welcome back {username}</h2>
+        <h3 className={styles.pageSubtitle}>Your datasets:</h3>
+        <ul className={styles.datasetList}>
+          {datasets.map((dataset, index) => (
+            <li key={dataset.firestoreId}>
+              <DatasetCard dataset={dataset} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+
+  );
+};
+
+export default ProfilePage;
